4f97c84ec06e5165102cc5e74cf46007954df782
Lennert Bontinck <lennertbontinck@live.be>
2019-05-12 21:13:44 +0200

    Evaluatietool begin: landing page

---
+++ evaluatietool/index.php
@@ -0,0 +1,46 @@
+<?php
+//header voorzien
+include 'layout/header.php';
+?>
+    <div class="p-5 mb-4 bg-primary text-white">
+        <h1 class="mb-4">Bachelorproef onderzoek: afbeeldingskwaliteit</h1>
+
+        <p class="mb-2">Hallo!</p>
+        <p class="mb-2">Bedankt voor de interesse om te willen deelnemen aan dit bachelorproef onderzoek naar
+            afbeeldingskwaliteit.</p>
+        <p class="mb-2">Dit onderzoek bestaat uit het beoordelen van vier kenmerken van een afbeelding met een score van
+            één tot vijf. Er zullen in totaal vier verschillende afbeeldingsformaten geëvalueerd worden welke niet
+            gekend zijn door de participant van dit onderzoek. Meer info volgt aan de hand van een korte
+            introductie video op de volgende pagina. </p>
+        <p class="mb-2">Voor dit onderzoek zullen de volgende gegevens van u bewaard worden:
+        <ul>
+            <li>
+                Geslacht
+            </li>
+            <li>
+                Leeftijd
+            </li>
+            <li>
+                Expertise binnen het onderzoek domein
+            </li>
+            <li>
+                Of u al dan niet kleurenblind bent
+            </li>
+            <li>
+                Of u al dan niet slechtziend bent
+            </li>
+            <li>
+                De scores die u aan de afbeeldingen toekent
+            </li>
+        </ul></p>
+        <p class="mb-2">U zal in totaal 60 afbeeldingen beoordelen. De geschatte duur van dit onderzoek bedraagd 30 tot 45 minuten.</p>
+
+
+
+    </div>
+    <button type="button" class="btn btn-outline-primary w-100">Ik ben akkoord met de gegevensverwerking en doe mee aan
+        het onderzoek!
+    </button>
+<?php
+//footer voorzien
+include 'layout/footer.php';
